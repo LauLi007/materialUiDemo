67c7356f5c6003a63753391e96dc09ef3ab8517c
lauli <lindstedt.laura@gmail.com>
2019-10-11 09:40:56 +0300

    postdata.js - titles modified

--- src/Postdata.js
+++ src/Postdata.js
@@ -1,6 +1,6 @@
 const postdata = [
     {
-        title: "My first post",
+        title: "All about coffee",
         image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/15/16/china-coffee-cup.jpg?w968h681",
         description: "This is my first post",
         url: "https://www.google.com",
@@ -8,7 +8,7 @@ const postdata = [
     },
 
     {
-        title: "My second post",
+        title: "Cooking with coffee",
         image: "https://www.incimages.com/uploaded_files/image/970x450/getty_938993594_401542.jpg",
         description: "This is my second post",
         url: "https://www.bc.fi",
@@ -16,21 +16,21 @@ const postdata = [
     },
 
     {
-        title: "My third post",
+        title: "Italian coffee",
         image: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555306143/shape/mentalfloss/istock_43355764_small.jpg",
         description: "This is my third post",
         url: "https://www.bc.fi",
         text: "Liquorice dragée cupcake ice cream toffee. Cheesecake pudding macaroon candy pudding chocolate cake jelly. Topping cookie candy liquorice gingerbread soufflé sesame snaps. Sweet roll sesame snaps apple pie chocolate. Lollipop macaroon soufflé. Lemon drops cake dragée cake gummies caramels topping carrot cake liquorice. Cake carrot cake tart chocolate bar chocolate macaroon candy canes sesame snaps marshmallow. Dessert caramels tart icing topping liquorice macaroon. Liquorice cotton candy gummies. Ice cream tart danish dragée. Chocolate cake danish sweet chocolate cake. Chocolate tiramisu cupcake caramels muffin. Cotton candy cake dessert pudding liquorice. Lemon drops lollipop danish pastry sugar plum gingerbread caramels."
     },
     {
-        title: "My fourth post",
+        title: "The Best Cafeterias in Town",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9yfojEU_UTtjmxLaAQ0Ycf99UYWOuuoNMhoEaUWkRDjC_9TPV",
         description: "This is my fourth post",
         url: "https://www.bc.fi",
         text: "Liquorice dragée cupcake ice cream toffee. Cheesecake pudding macaroon candy pudding chocolate cake jelly. Topping cookie candy liquorice gingerbread soufflé sesame snaps. Sweet roll sesame snaps apple pie chocolate. Lollipop macaroon soufflé. Lemon drops cake dragée cake gummies caramels topping carrot cake liquorice. Cake carrot cake tart chocolate bar chocolate macaroon candy canes sesame snaps marshmallow. Dessert caramels tart icing topping liquorice macaroon. Liquorice cotton candy gummies. Ice cream tart danish dragée. Chocolate cake danish sweet chocolate cake. Chocolate tiramisu cupcake caramels muffin. Cotton candy cake dessert pudding liquorice. Lemon drops lollipop danish pastry sugar plum gingerbread caramels."
     },
     {
-        title: "My fifth post",
+        title: "Coffee and sweets",
         image: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/AN275-cup-of-coffee-732x549-Thumb.jpg?w=756&h=567",
         description: "This is my third post",
         url: "https://www.bc.fi",
